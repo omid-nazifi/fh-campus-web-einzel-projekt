54742d43864e4199317f7baa230385e316506f70
Omidreza Nazifi <omid.nazifi@gmail.com>
2021-10-10 19:13:25 +0200

    use gzip

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,15 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  plugins: [
+      new CompressionPlugin({
+          test: /\.js$|\.css$|\.html$/,
+          algorithm: "gzip",
+          threshold: 1024,
+          minRatio: 0.8
+      }),
+  ],
 });
